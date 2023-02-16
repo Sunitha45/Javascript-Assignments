1040242916dd08e5f5759b71ac020ec6da27e4de
Sunitha45 <123886147+Sunitha45@users.noreply.github.com>
2023-02-16 23:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
+
+//string//
+const d="amudala";
+const e="suni";
+const f=d+e;
+console.log(f);
+
+//Boolean//
+var x=0;
+if(x==0)
+ console.log(1);
+ else{
+    cconsole.log(0);
+ }
+
+ //undefined//
+ console.log(j);
+ let j=10;
+
+ //null//
+  var k=null;
+  console.log(typeof k);
+
